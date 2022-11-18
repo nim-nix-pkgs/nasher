29406ab7c1791f79bcb968c451a7935a9ce483f7
hugosenari <hugosenari@gmail.com>
2022-11-18 13:30:49 -0300

    chore: re index nasher

--- master/flake.nix
+++ master/flake.nix
@@ -37,6 +37,14 @@
   inputs."nwnt".inputs.nixpkgs.follows = "nixpkgs";
   inputs."nwnt".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."blarg".owner = "nim-nix-pkgs";
+  inputs."blarg".ref   = "master";
+  inputs."blarg".repo  = "blarg";
+  inputs."blarg".dir   = "0_1_0";
+  inputs."blarg".type  = "github";
+  inputs."blarg".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."blarg".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
